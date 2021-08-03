952eefc426cfa75538c7c573bbf189a8caff01e4
Abdul Haseeb <ahaseeb0809@gmail.com>
2021-08-03 17:52:56 +0530

    Changed the theme to dark

--- js/game.js
+++ js/game.js
@@ -35,7 +35,7 @@ export default class Game{
     findWinningCombinations(){
         const WinningCombinations= [
             [0,1,2],
-            [3.4,5],
+            [3,4,5],
             [6,7,8],
             [0,3,6],
             [1,4,7],
